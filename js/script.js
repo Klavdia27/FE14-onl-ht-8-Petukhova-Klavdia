ad434d108a6e62bcaa823cedaa6311684682c93e
Klavdia Petukhova <kklavdia27@gmail.com>
2021-07-26 12:21:56 +0300

    add homework2

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.slaider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    speed: 100,
+});
+});
+
+$(document).ready(function(){
+  $('.container-slaider').slick({
+    arrows: false,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    speed: 100,
+    
+});
+});
